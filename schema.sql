d50fd005ec7a0a981eda91b7841b8fcfcc33b037
shubham <shubham.d23896@gmail.com>
2019-11-21 11:11:17 +0530

    addr

---
+++ schema.sql
@@ -0,0 +1,5 @@
+create table emp(no integer primary key auto_increment,name varchar(20));
+
+insert into emp (name) values("shubham");
+insert into emp (name) values("amol");
+insert into emp (name) values("rushi");
